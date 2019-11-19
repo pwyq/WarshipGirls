5edffaaaa3db736b040f44afc1314b1d9e6b75c9
pwyq <y433wu@edu.uwaterloo.ca>
2019-11-19 07:53:01 +0100

    add ordance page

--- js/main-image.js
+++ js/main-image.js
@@ -1,9 +1,16 @@
 // <!-- images resolution 1600 x 1200 -->
-var box_images = [
+var box_lineup_images = [
   "images/uss_navy.jpg",
   "images/US_fleet_at_Majuro_Atoll_1944.jpg"
 ];
+var lineup_size = box_lineup_images.length
+var lineup_x = Math.floor(lineup_size*Math.random())
+document.getElementById('box-lineup').src=box_lineup_images[lineup_x];
 
-var size = box_images.length
-var x = Math.floor(size*Math.random())
-document.getElementById('box-lineup').src=box_images[x];
+var box_ordance_images = [
+  "images/BB-57.jpg",
+  "images/BB-63.jpg"
+];
+var ordance_size = box_ordance_images.length
+var ordance_x = Math.floor(ordance_size*Math.random())
+document.getElementById('box-ordance').src=box_ordance_images[ordance_x];
