31f84d50b18786e702b59ae88d32844659abcab4
pwyq <y433wu@edu.uwaterloo.ca>
2019-11-24 20:02:51 +0100

    praise 6SS!

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,11 @@
 # File              : run.sh
 # Author            : Yanqing Wu <meet.yanqing.wu@gmail.com>
 # Date              : 21.11.2019
-# Last Modified Date: 22.11.2019
+# Last Modified Date: 24.11.2019
 # Last Modified By  : Yanqing Wu <meet.yanqing.wu@gmail.com>
 
 # `$ sudo crontab -e` to auto schedule
 cd /home/pwyq/github/WarshipGirls/scripts
-date >> nov.log
-python3 kitchen_popularity.py >> nov.log
-python3 kitchen_popularity_new.py >> nov.log
+date > nov.log
+python3 kitchen_popularity.py 2>&1 | tee -a nov.log
+python3 kitchen_popularity_new.py 2>&1 | tee -a nov.log
