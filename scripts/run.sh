e8eff239f63e6a1983b6d0a1c581c4799b0110ed
pwyq <y433wu@edu.uwaterloo.ca>
2019-11-28 15:51:24 +0100

    ..

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,11 +2,24 @@
 # File              : run.sh
 # Author            : Yanqing Wu <meet.yanqing.wu@gmail.com>
 # Date              : 21.11.2019
-# Last Modified Date: 24.11.2019
+# Last Modified Date: 27.11.2019
 # Last Modified By  : Yanqing Wu <meet.yanqing.wu@gmail.com>
 
 # `$ sudo crontab -e` to auto schedule
 cd /home/pwyq/github/WarshipGirls/scripts
-date > nov.log
-python3 kitchen_popularity.py 2>&1 | tee -a nov.log
-python3 kitchen_popularity_new.py 2>&1 | tee -a nov.log
+
+file=auto.log
+date > $file
+minimum_size=10000
+actual_size=$(wc -c <"$file")
+
+while [ $actualsize -le $minimumsize ]; do
+    #echo "running scripts..." | tee -a $file
+    echo "running scripts..." > $file
+    #python3 kitchen_popularity.py | tee -a $file
+    python3 kitchen_popularity.py >> $file
+    #python3 kitchen_popularity_new.py | tee -a $file
+    python3 kitchen_popularity_new.py >> $file
+done
+
+notify-send "WGR auto scripting is done!"
