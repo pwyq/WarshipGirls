151093998ab35dabce4f094b2f4d2f12671911cf
pwyq <y433wu@edu.uwaterloo.ca>
2019-12-21 23:57:15 +0100

    update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,13 +2,14 @@
 # File              : run.sh
 # Author            : Yanqing Wu <meet.yanqing.wu@gmail.com>
 # Date              : 21.11.2019
-# Last Modified Date: 30.11.2019
+# Last Modified Date: 21.12.2019
 # Last Modified By  : Yanqing Wu <meet.yanqing.wu@gmail.com>
 
 # `$ sudo crontab -e` to auto schedule
 cd /home/pwyq/github/WarshipGirls/scripts
 
 file=auto.log
+file2=kiss.log
 date > $file
 #minimum_size=10000
 #actual_size=$(wc -c <"$file")
@@ -24,6 +25,8 @@ python3 kitchen_popularity.py >> $file
     #python3 kitchen_popularity_new.py | tee -a $file
 python3 kitchen_popularity_new.py >> $file
     #python3 kitchen_popularity_new.py
+
+python3 auto_favorability.py >> $file2
 
 #    actual_size=$(wc -c <"$file")
 #done
